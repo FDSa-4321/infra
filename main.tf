e1c13a04a315a5e3f84523430004ffa69a6f41c1
Hari Kishore <harikishore0405@gmail.com>
2023-12-01 11:54:28 +0530

    Vpc file created

--- main.tf
+++ main.tf
@@ -16,46 +16,5 @@ terraform {
 provider "aws" {
 
   region = "us-east-1" # define region as per your account
-
 }
 
-resource "aws_vpc" "main" {
- 
- cidr_block = "10.0.0.0/16"
- 
- tags = {
-   Name = "Project VPC"
- }
-}
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.main.id
- tags = {
-   Name = "Project VPC IG"
- }
-}
-
-resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.main.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.gw.id
- }
- tags = {
-   Name = "2nd Route Table"
- }
-}
-
-resource "aws_route_table_association" "public_subnet_asso" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.second_rt.id
-}
