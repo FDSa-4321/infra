f4c2d3d820895f8f38cfbfc681ebcf416bf2b7ce
Hari Kishore <harikishore0405@gmail.com>
2023-12-02 11:55:45 +0530

    Resource Ec2 added

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+  key_name      = "awskey"
+
+  subnet_id                   = aws_subnet.main.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+
+  apt install nginx -y
+  echo "<h1>$(curl https://api.kanye.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
+  systemctl enable nginx
+  systemctl start nginx
+  EOF
+
+  tags = {
+    "Name" : "Kanye"
+  }
+}
