9c384a9985f5fbe689976d0cedf5db0ddc0228f6
Hari Kishore <harikishore0405@gmail.com>
2023-12-05 12:48:41 +0530

    .gitignore added

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web_instance" {
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   associate_public_ip_address = true
 
-  user_data = << -EOF
+  user_data = <<-EOF
   #!/bin/bash -ex
 
   apt install nginx -y
